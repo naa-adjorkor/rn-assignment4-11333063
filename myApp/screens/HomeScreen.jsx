b239baaa450958b430bb0c8bb7164a34aa5d1065
naa-adjorkor <naakrong_adjei@st.ug.edu.gh>
2024-06-15 23:44:34 +0000

    updated HomeScreen(in progress)

--- myApp/screens/HomeScreen.jsx
+++ myApp/screens/HomeScreen.jsx
@@ -1,13 +1,60 @@
-import { View, Text } from 'react-native'
+import { View, Text,StyleSheet,Image, TextInput } from 'react-native'
 import React from 'react'
+
 
 
 export default function HomeScreen({route}) {
   const {name,email} = route.params;
+
   return (
-    <View>
-      <Text>Welcome {name}</Text>
-      <Text>Welcome {email}</Text>
+    <View style={styles.home}>
+      <View>
+        <Text style={styles.name}>Naa</Text>
+        <Text style={styles.email}>naa@gmail.com</Text>
+        <Image source={require('../assets/images/apple.png')} style={styles.user}/>
+      </View>
+      
+
+        <TextInput
+            style={styles.input}
+            placeholder="Search jobs or position"
+            placeholderTextColor={'#AFB0B6'}
+        />
+      
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  home:{
+    top:64,
+    left:24,
+  },
+  name:{
+    fontSize: 24,
+    fontWeight: 700,
+  },
+  email:{
+    color: '#95969D',
+    fontSize:20,
+    fontWeight: 400,
+  },
+  user:{
+    width: 54,
+    height: 54,
+    left:300,
+    top:-52,
+  },
+  input:{
+    height:50,
+    width:265,
+    borderWidth:1,
+    borderColor:'#AFB0B6',
+    padding:12,
+    fontSize:14,
+    fontWeight:500,
+    color:'#000000',
+    borderRadius:12,
+    fontSize:15,
+  },
+})
